704f02f3398fb4e8539855f616ac472496605d46
kaiquedevesa1 <kaikedaeves@hotmail.com>
2025-05-14 20:00:59 -0300

    add script enter accaunt

---
+++ app.py
@@ -0,0 +1,20 @@
+import webbrowser
+import pyautogui
+from time import sleep 
+
+
+webbrowser.open('https://www.tiktok.com/')
+sleep(5)
+pyautogui.click(957,412, duration=2)
+sleep(5)
+pyautogui.click(985,276, duration=2)
+sleep(5)
+pyautogui.click(823,319, duration=2)
+sleep(5)
+pyautogui.write("kaiquealencardevesa1234@outlook.com")
+sleep(5)
+pyautogui.click(817,369, duration=2)
+sleep(5)
+pyautogui.write("ade345he@")
+sleep(5)
+pyautogui.click(943,465, duration=2)
---
+++ mouse-tracker.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+
+try:
+    while True:
+        x,y = pyautogui.position()
+        print(x, y)
+except KeyboardInterrupt:
+        print('\nDone.')
+       
+
